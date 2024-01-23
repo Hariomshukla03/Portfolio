5445714067204c3e4a0c9579f4bb3c3c51f53279
HARIOM SHUKLA <hariom09shukla@gmail.com>
2024-01-23 18:22:41 +0530

    .

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ btn.addEventListener("click",()=>{
     sld_nav.classList.toggle("slide_nav");
     sld_nav.style=fontSize="3.5rem" ;
 });
+let livebtn=document.querySelector("#livebtn");
+livebtn.addEventListener(("click"),()=>{
+    open("https://hariomshukla03.github.io/MovieMagnet/")
+})
 
